68a3e052851219d2bd3311c6c10980da8f5f8e25
Borjita <borja.fierro@gmail.com>
2022-10-23 01:34:50 +0200

    otro push

--- ProyectoNapba/Assets/Codigo/Spawns.cs
+++ ProyectoNapba/Assets/Codigo/Spawns.cs
@@ -9,14 +9,29 @@ public class Spawns : MonoBehaviour
     public int numSpawn;
 
     public GameObject[] enemigos;
+    /*
+    public GameObject[] enemigosDobles;
+    public GameObject[] enemigosTriples;
+    public GameObject[] enemigosCuadruples;
+    */
 
-    public float esperaInicialEnemigosTipo1 = 5; //segundos antes de el primer spawn y espera base para el resto de la partida
-    public float esperaInicialEnemigosTipo2 = 30;
-    
-    private float tiempo;
+    int cantidadEnemigos1;
+    int cantidadEnemigos2;
+    int cantidadEnemigos3;
+    int cantidadEnemigos4;
+    int cantidadEnemigos5;
+
+    public float esperaInicialEnemigosTipo1; //segundos antes de el primer spawn y espera base para el resto de la partida
+    public float esperaInicialEnemigosTipo2;
+    public float esperaInicialEnemigosTipo3; 
+    public float esperaInicialEnemigosTipo4;
+    public float esperaInicialEnemigosTipo5; 
+
     float esperaEnemigosTipo1; //segundos entre cada spawn
     float esperaEnemigosTipo2;
-    float dificultad = 5;
+    float esperaEnemigosTipo3;
+    float esperaEnemigosTipo4;
+    float esperaEnemigosTipo5;
 
 
     void Start()
@@ -24,11 +39,18 @@ public class Spawns : MonoBehaviour
 
         esperaEnemigosTipo1 = esperaInicialEnemigosTipo1;
         esperaEnemigosTipo2 = esperaInicialEnemigosTipo2;
+        esperaEnemigosTipo3 = esperaInicialEnemigosTipo3;
+        esperaEnemigosTipo4 = esperaInicialEnemigosTipo4;
+        esperaEnemigosTipo5 = esperaInicialEnemigosTipo5;
 
+        cantidadEnemigos1 = 1;
+        cantidadEnemigos2 = 1;
+        cantidadEnemigos3 = 1;
+        cantidadEnemigos4 = 1;
+        cantidadEnemigos5 = 1;
 
         StartCoroutine(SpawnEnemigosTipo1());
-        StartCoroutine(SpawnEnemigosTipo2());
-        
+        StartCoroutine(SpawnEnemigosTipo2());      
     }
     IEnumerator SpawnEnemigosTipo1()
     {
@@ -36,18 +58,43 @@ public class Spawns : MonoBehaviour
 
         Vector3 spawnPosition1 = new Vector3(0, 0, 0);
         spawnPosition1 = new Vector3(transform.position.x, transform.position.y, 0);
-        EnemigoIA spawn = enemigos[0].GetComponent<EnemigoIA>();
-        spawn.puntoSpawn = numSpawn;
-        GameObject enemigo1 = Instantiate(enemigos[0], spawnPosition1, gameObject.transform.rotation);
+
+        if (cantidadEnemigos1 == 1)
+        {
+            EnemigoIA spawn = enemigos[0].GetComponent<EnemigoIA>();
+            spawn.puntoSpawn = numSpawn;
+            GameObject enemigo1 = Instantiate(enemigos[0], spawnPosition1, gameObject.transform.rotation);
+            GameObject enemigo2 = Instantiate(enemigos[0], spawnPosition1 + new Vector3(0.1f, 0.1f, 0), gameObject.transform.rotation);
+        }
+        else if (cantidadEnemigos1 == 2)
+        {
+            EnemigoIA spawn = enemigos[0].GetComponent<EnemigoIA>();
+            spawn.puntoSpawn = numSpawn;
+            GameObject enemigo1 = Instantiate(enemigos[0], spawnPosition1, gameObject.transform.rotation);
+        }
+        else if (cantidadEnemigos1 == 3)
+        {
+            EnemigoIA spawn = enemigos[0].GetComponent<EnemigoIA>();
+            spawn.puntoSpawn = numSpawn;
+            GameObject enemigo1 = Instantiate(enemigos[0], spawnPosition1, gameObject.transform.rotation);
+        }
+        else if (cantidadEnemigos1 == 4)
+        {
+            EnemigoIA spawn = enemigos[0].GetComponent<EnemigoIA>();
+            spawn.puntoSpawn = numSpawn;
+            GameObject enemigo1 = Instantiate(enemigos[0], spawnPosition1, gameObject.transform.rotation);
+        }
 
         if (esperaEnemigosTipo1 > 5)
         {
             esperaEnemigosTipo1 = esperaEnemigosTipo1 * (1 - reduccionTiempo);
-            Debug.Log(":" + esperaEnemigosTipo1);
+            
         }
         else
         {
-            esperaEnemigosTipo1 = 5;
+            esperaEnemigosTipo1 = esperaInicialEnemigosTipo1;
+            Debug.Log("dobles");
+            cantidadEnemigos1++;
         }
 
         StartCoroutine(SpawnEnemigosTipo1());
