e31d6f9fec928cdc8e27a801f5ce020f5fc02100
AlbertoGarrote <sugaryspider822@gmail.com>
2022-10-24 22:35:59 +0200

    escenas de muerte

---
+++ ProyectoNapba/Assets/Codigo/salir.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salir : MonoBehaviour
+{
+   
+   public void Salir2(){
+        Application.Quit();
+        
+    }
+}
---
+++ ProyectoNapba/Assets/Codigo/salir1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salir1 : MonoBehaviour
+{
+  public void Salir3(){
+        Application.Quit();
+        
+    }
+}
